5afc19e4d27b50846d4187ebe608b4235adda728
Conrad Chase Metzger (Cq) <cqmailofficial@gmail.com>
2025-12-28 23:55:17 -0800

    helpers.h: Reformatted and removed custom implementation print functions.
    markov.h: Fixed (sorta) bug in makeCleanSentences and changed typedefs.
    
    main.cpp: Now using rapidcsv to read dictionary file and generate text
    based on that.

--- helpers.h
+++ helpers.h
@@ -1,6 +1,7 @@
 #ifndef LIB_CQ_MENTAL_HELPERS_H
 #define LIB_CQ_MENTAL_HELPERS_H
 
+#include <cstdio>
 #include <string>
 #include <vector>
 #include <print>
@@ -33,18 +34,18 @@ ElemType mostFrequentElemen(StdVector<ElemType> &arr)
 	for (int i = 0; i < n; i++) freq[arr[i]]++;
 
 	int maxCnt = 0;
-	ElemType res;
+	ElemType result;
 
 	for (const auto &[val, cnt] : freq)
 	{
 		if (maxCnt < cnt)
 		{
-			res = val;
+			result = val;
 			maxCnt = cnt;
 		}
 	}
 
-	return res;
+	return result;
 }
 
 // Trim leading whitespace
@@ -72,166 +73,113 @@ void trim(StdString &s)
 
 namespace Cq
 {
-
-namespace Mental
-{
-struct Point2D
-{
-	float x = 0.0f, y = 0.0f;
-};
-
-using Point2DVector = StdVector<Point2D>;
-
-float euclideanDistance(Point2D p1, Point2D p2)
-{
-	Point2D diff{std::pow(p1.x - p2.x, 2.0f), std::pow(p1.y - p2.y, 2.0f)};
-	auto sum = diff.x + diff.y;
-	return std::sqrt(sum);
-}
-
-template<typename LabelType>
-LabelType knnPredict(const Point2DVector &trainingData, const StdVector<LabelType> &trainingLabels, Point2D testPoint, int k)
-{
-	StdVector<std::pair<float, LabelType>> dists;
-
-	for(int i = 0; i < trainingData.size(); ++i)
+	namespace Mental
 	{
-		const auto d = euclideanDistance(testPoint, trainingData[i]);
-		dists.push_back({d, trainingLabels[i]});
-	}
-
-	std::sort(std::begin(dists), std::end(dists), [](auto d1, auto d2) {
-		return d1.first < d2.first;
-	});
-
-	StdVector<LabelType> knnLabels;
-	knnLabels.reserve(k);
-
-	for(int i = 0; i < k; ++i)
-	{
-		knnLabels.push_back(dists[i].second);
-	}
-
-	return mostFrequentElement(knnLabels);
-}
-
-}
-
-void test(void)
-{
-	std::println("blah testing...");
-}
-
-template<typename T>
-void print(const T &t)
-{
-	std::print("{}", t);
-}
-
-template<typename FirstType, typename SecondType>
-void print(const StdPair<FirstType, SecondType> &p)
-{
-	print("(");
-	print(p.first);
-	print(", ");
-	print(p.second);
-	print(")");
-}
-
-template<typename T>
-void print(const StdVector<T> &v)
-{
-	std::print("[");
-	int n = 0;
-	for(const auto &e : v)
-	{
-		print(e);
-		if(n < v.size() - 1)
+		struct Point2D
 		{
-			std::print(", ");
-			++n;
+			float x = 0.0f, y = 0.0f;
+		};
+
+		using Point2DVector = StdVector<Point2D>;
+
+		float euclideanDistance(Point2D p1, Point2D p2)
+		{
+			Point2D diff{std::pow(p1.x - p2.x, 2.0f), std::pow(p1.y - p2.y, 2.0f)};
+			auto sum = diff.x + diff.y;
+			return std::sqrt(sum);
+		}
+
+		template<typename LabelType>
+		LabelType knnPredict(const Point2DVector &trainingData, const StdVector<LabelType> &trainingLabels, Point2D testPoint, int k)
+		{
+			StdVector<std::pair<float, LabelType>> dists;
+
+			for(int i = 0; i < trainingData.size(); ++i)
+			{
+				const auto d = euclideanDistance(testPoint, trainingData[i]);
+				dists.push_back({d, trainingLabels[i]});
+			}
+
+			std::sort(std::begin(dists), std::end(dists), [](auto d1, auto d2) {
+				return d1.first < d2.first;
+			});
+
+			StdVector<LabelType> knnLabels;
+			knnLabels.reserve(k);
+
+			for(int i = 0; i < k; ++i)
+			{
+				knnLabels.push_back(dists[i].second);
+			}
+
+			return mostFrequentElement(knnLabels);
 		}
 	}
-	std::print("]");
-}
 
-template<typename KeyType, typename ValueType>
-void print(const StdMap<KeyType, ValueType> &m)
-{
-	print("{");
-	int n = 0;
-	for(const auto &p : m)
+	void test(void)
+	{
+		std::println("blah testing...");
+	}
+
+	template<typename T>
+	void print(const T &t)
+	{
+		std::print("{}", t);
+	}
+
+	template<typename FirstType, typename SecondType>
+	void print(const StdPair<FirstType, SecondType> &p)
+	{
+		print("(");
+		print(p.first);
+		print(", ");
+		print(p.second);
+		print(")");
+	}
+
+	template<typename T>
+	void print(const StdVector<T> &v)
+	{
+		std::print("{}", v);
+	}
+
+	template<typename KeyType, typename ValueType>
+	void print(const StdMap<KeyType, ValueType> &m)
+	{
+		std::print("{}", m);
+	}
+
+	template<typename T>
+	void println(const T &t)
+	{
+		std::println("{}", t);
+	}
+
+	template<typename FirstType, typename SecondType>
+	void println(const StdPair<FirstType, SecondType> &p)
 	{
 		print(p);
-		if(n < m.size() - 1)
-		{
-			print(", ");
-			++n;
-		}
+		print("\n");
 	}
-	print("}");
-}
 
-template<typename T>
-void println(const T &t)
-{
-	std::print("{}\n", t);
-}
-
-template<typename FirstType, typename SecondType>
-void println(const StdPair<FirstType, SecondType> &p)
-{
-	print(p);
-	print("\n");
-}
-
-template<typename T>
-void println(const StdVector<T> &v)
-{
-	print(v);
-	std::print("\n");
-}
-
-template<typename KeyType, typename ValueType>
-void println(const StdMap<KeyType, ValueType> &m)
-{
-
-	print("{\n");
-	int n = 0;
-	for(const auto &p : m)
+	template<typename T>
+	void println(const StdVector<T> &v)
 	{
-		print(p);
-		if(n < m.size() - 1)
-		{
-			print(", ");
-			++n;
-		}
-
-		std::print("\n");
+		std::println("{}", v);
 	}
-	print("}\n");
-}
 
-template<typename KeyType, typename ValueType>
-void println(const StdUnorderedMap<KeyType, ValueType> &m)
-{
-
-	print("{\n");
-	int n = 0;
-	for(const auto &p : m)
+	template<typename KeyType, typename ValueType>
+	void println(const StdMap<KeyType, ValueType> &m)
 	{
-		print(p);
-		if(n < m.size() - 1)
-		{
-			print(", ");
-			++n;
-		}
 
-		std::print("\n");
+		std::println("{}", m);
 	}
-	print("}\n");
-}
 
+	template<typename KeyType, typename ValueType>
+	void println(const StdUnorderedMap<KeyType, ValueType> &m)
+	{
+		std::println("{}", m);
+	}
 }
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,86 +1,138 @@
+#include <cstdint>
+#include <ios>
 #include <print>
 #include <algorithm>
 #include <iterator>
 #include <random>
+#include <set>
+
+#include <rapidcsv.h>
 
 #include "helpers.h"
 #include "markov.h"
 
 using namespace Cq;
 
-const StdString TESTSTR = "Cq here hello world here Cq here good world";
-
-const StdStringVector testLines = {
-//	"This lines of code generate a general purpose Markov model from a given list of cleaned words (cleaned_text).",
-//	"The function considers an n_gram here the default n value is 3 so itb",
-//	"ll consider trigrams to create word pairs and then builds a Markov model representing the transitions between these pairs.",
-	"Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, and what is the use of a book, thought Alice without pictures or conversations?"
-	"So she was considering in her own mind (as well as she could, for the hot day made her feel very sleepy and stupid), whether the pleasure of making a daisy-chain would be worth the trouble of getting up and picking the daisies, when suddenly a White Rabbit with pink eyes ran close by her."
-	"There was nothing so very remarkable in that; nor did Alice think it so very much out of the way to hear the Rabbit say to itself, Oh dear! Oh dear! I shall be late! (when she thought it over afterwards, it occurred to her that she ought to have wondered at this, but at the time it all seemed quite natural); but when the Rabbit actually took a watch out of its waistcoat-pocket, and looked at it, and then hurried on, Alice started to her feet, for it flashed across her mind that she had never before seen a rabbit with either a waistcoat-pocket, or a watch to take out of it, and burning with curiosity, she ran across the field after it, and fortunately was just in time to see it pop down a large rabbit-hole under the hedge."
-	"In another moment down went Alice after it, never once considering how in the world she was to get out again."
-	"The rabbit-hole went straight on like a tunnel for some way, and then dipped suddenly down, so suddenly that Alice had not a moment to think about stopping herself before she found herself falling down a very deep well."
-	"Either the well was very deep, or she fell very slowly, for she had plenty of time as she went down to look about her and to wonder what was going to happen next. First, she tried to look down and make out what she was coming to, but it was too dark to see anything; then she looked at the sides of the well, and noticed that they were filled with cupboards and book-shelves; here and there she saw maps and pictures hung upon pegs. She took down a jar from one of the shelves as she passed; it was labelled ORANGE MARMALADE, but to her great disappointment it was empty: she did not like to drop the jar for fear of killing somebody underneath, so managed to put it into one of the cupboards as she fell past it."
-	"Well! thought Alice to herself, after such a fall as this, I shall think nothing of tumbling down stairs! How brave they ll all think me at home! Why, I wouldnt say anything about it, even if I fell off the top of the house! (Which was very likely true.)"
-	"Down, down, down. Would the fall never come to an end? I wonder how many miles Ive fallen by this time? she said aloud. I must be getting somewhere near the centre of the earth. Let me see: that would be four thousand miles down, I think (for, you see, Alice had learnt several things of this sort in her lessons in the schoolroom, and though this was not a very good opportunity for showing off her knowledge, as there was no one to listen to her, still it was good practice to say it over) yes, thats about the right distance but then I wonder what Latitude or Longitude Ive got to? (Alice had no idea what Latitude was, or Longitude either, but thought they were nice grand words to say.)"
-	"Presently she began again. I wonder if I shall fall right through the earth! How funny it ll seem to come out among the people that walk with their heads downward! The Antipathies, I think (she was rather glad there was no one listening, this time, as it didnt sound at all the right word) but I shall have to ask them what the name of the country is, you know. Please, Maam, is this New Zealand or Australia? (and she tried to curtsey as she spoke fancy curtseying as youre falling through the air! Do you think you could manage it?) And what an ignorant little girl shell think me for asking! No, itll never do to ask: perhaps I shall see it written up somewhere."
+struct Word
+{
+	StdString name;
+	StdStringVector definitions;
 };
 
-int main(void)
+bool operator<(const Word &w1, const Word &w2)
 {
-	const int N_GRAM = 3;
-	const int NUM_ITERS = 2048;
+	return w1.name < w2.name;
+}
 
-	std::println("# of lines = {}", testLines.size());
+int main(int argc, char *argv[])
+{
+	if(argc < 2)
+	{
+		std::println("Enter a dictionary filename!!!");
+		return -1;
+	}
 
-	auto cleanSentences = makeCleanSentences(testLines);
-	std::println("# of sentences = {}", cleanSentences.size());
+	const auto DICT_FILENAME = argv[1];
+//	const auto DICT_FILENAME = "testdict.csv";
+//	const auto DICT_FILENAME = "dict.csv";
+	rapidcsv::Document dictDoc(DICT_FILENAME);
 
-	auto cleanWords = makeCleanWords(cleanSentences); //cleanSentences[0]});
-	std::println("# of words = {}", cleanWords.size());
-//	println(cleanWords);
-	auto model = makeMarkovModel(cleanWords, N_GRAM);
-	std::println("# of states = {}", model.size());
-	//println(model);
+	StdStringVector words = dictDoc.GetColumn<StdString>("word");
+
+	std::println("# of words (non-unique) = {}", words.size());
+	std::set<Word, std::less<>> dict;
+
+	for(int i = 0; i < words.size(); ++i)
+	{
+		auto def = dictDoc.GetCell<StdString>("definition", i);
+		auto found = dict.find({words[i], {}});
+		if(found != dict.end())
+		{
+			auto newDefs = found->definitions;
+			newDefs.push_back(def);
+			dict.erase(*found);
+			dict.insert({words[i], newDefs});
+		} else dict.insert({words[i], {def}});
+	}
+
+	std::println("# of dictionary words = {}", dict.size());
+
+	std::println("Cleaning up definitions...");
+	StdMap<StdString, StdStringVector> wordToCleanDefWords;
+	for(const auto &w : dict)
+	{
+		auto cleanDefSentences = makeCleanSentences(w.definitions);
+		auto cleanDefWords = makeCleanWords(cleanDefSentences);
+		wordToCleanDefWords[w.name] = cleanDefWords;
+	}
+
+	std::println("Done cleaning definitions");
+
 	std::random_device rd;
 	std::default_random_engine rne(rd());
+	
+	const int N_GRAM = 2;
+	auto getRandDictWord = [&rne, &dict](void){
+		StdVector<Word> randWords;
+		std::sample(dict.cbegin(), dict.cend(), std::back_inserter(randWords), 1, rne);
+		return randWords[0];
+	};
 
-	auto getRandState = [&rne, &model]() {
+	auto getRandModel = [&rne, &wordToCleanDefWords, &getRandDictWord](void) {
+		auto randWord = getRandDictWord();
+		return std::make_pair(randWord, makeMarkovModel(wordToCleanDefWords[randWord.name], N_GRAM));
+	};
+
+	auto getRandState = [&rne](const MarkovModel &model) {
 		MarkovModelStatePairVector randStates;
 		std::sample(model.cbegin(), model.cend(), std::back_inserter(randStates), 1, rne);
 		return randStates[0];
 	};
-	StdString curState = getRandState().first;
-	std::println("Starting state = {}", curState);
+
+	std::println("Picking a random word to generate a model from...");
+	MarkovModel curModel;
+	Word startingWord;
+	while(true)
+	{
+		startingWord = getRandDictWord();		
+		std::println("Trying {}...", startingWord.name);
+		curModel = makeMarkovModel(wordToCleanDefWords[startingWord.name], N_GRAM);
+		if(curModel.size() != 0) break;
+	}
+
+	std::println("Starting model word = {}", startingWord.name);
+	std::println("{}", curModel);
+	StdString curState = getRandState(curModel).first;
 //	StdString nextState = NO_WORD;
-	StdString text = curState + " ";
+	std::println("Initial state word = {}", curState);
 
 	if(wordCount(curState) != N_GRAM)
 	{
-		println("Error: make sure that the initial curState is the same as N_GRAM!!!");
+		std::println("Error: make sure that the initial curState is the same as N_GRAM!!!");
 		return -1;
 	}
 
+	StdString text = curState + " ";
 	int n = 0;
+	const int NUM_ITERS = 500;
+	std::println("Generating text...");
 	while(n < NUM_ITERS)
 	{
 		StdStringVector possibleWords;
-		const auto modelCurStateBegin = model[curState].cbegin();
-		const auto modelCurStateEnd = model[curState].cend();
+		const auto modelCurStateBegin = curModel[curState].cbegin();
+		const auto modelCurStateEnd = curModel[curState].cend();
 
-		possibleWords.reserve(std::distance(modelCurStateBegin, modelCurStateEnd));
+		possibleWords.reserve(curModel[curState].size());
 		std::transform(modelCurStateBegin, modelCurStateEnd, std::back_inserter(possibleWords), [](auto tran) {
 			return tran.first;
 		});
 
 		if(possibleWords.size() == 0)
 		{
-			curState = getRandState().first;
+			curState = getRandState(curModel).first;
 			++n;
 			continue;
 		}
-
-		//std::print("curState = \"{}\", possible next words = ", curState);
-		//println(possibleWords);
 
 		StdVector<float> probabilities;
 		probabilities.reserve(possibleWords.size());
